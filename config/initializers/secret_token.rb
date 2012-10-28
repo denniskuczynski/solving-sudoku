f70509cf56e163579ea761c49cf2489e0194d2ba
denniskuczynski <dennis.kuczynski@gmail.com>
2012-10-28 18:17:20 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SolvingSudoku::Application.config.secret_token = 'a40ba4d5c9f046c711c6910a0b0f7e08dcfe73b0a163cb3295d4ff4a274015ccde27de8b55550b1335b6def804f7033fe7e7c65c1bafc5d6a10ffb7aacd19335'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+SolvingSudoku::Application.routes.draw do   
+  root to: 'welcome#index'
+end
